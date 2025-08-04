70ae1dc17b7ccf22140aaf9dde5d117529bf0a46
Satyaki7 <dsatyaki956@gmail.com>
2025-08-04 12:50:02 +0530

    Update package dependencies and refactor MenuLinks component for improved image handling

--- src/components/MenuLinks.tsx
+++ src/components/MenuLinks.tsx
@@ -1,56 +1,64 @@
 import InfiniteMenu from "../Blocks/Components/InfiniteMenu/InfiniteMenu";
+import gmailImg from "./assets/gmail.webp";
+import linkedinImg from "./assets/005-linkedin.png";
+import discordImg from "./assets/discord.jpeg";
+import instagramImg from "./assets/002-instagram.png";
+import leetcodeImg from "./assets/leetcode.png";
+import portfolioImg from "./assets/webpage.png";
+import twitterImg from "./assets/twitter.jpg";
+import githubImg from "./assets/github.webp";
 
 function MenuLinks() {
-  const items = [
-    {
-      image: "./src/assets/gmail.webp",
-      link: "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=dsatyaki08@gmail.com",
-      title: "Mail",
-      description: "Reach me throught mail.",
-    },
-    {
-      image: "./src/assets/005-linkedin.png",
-      link: "https://www.linkedin.com/in/satyaki-das7/",
-      title: "Linkedin",
-      description: "This is where i post my work.",
-    },
-    {
-      image: "./src/assets/discord.jpeg",
-      link: "https://google.com/",
-      title: "Discord",
-      description: "This is where i chill with the community.",
-    },
-    {
-      image: "./src/assets/002-instagram.png",
-      link: "https://www.instagram.com/satyaki_das.official/",
-      title: "Instagram",
-      description: "Follow me on Instagram",
-    },
-    {
-      image: "./src/assets/leetcode.png",
-      link: "https://leetcode.com/u/satyaki7/",
-      title: "Leetcode",
-      description: "Want to see my DSA skills?",
-    },
-    {
-      image: "./src/assets/webpage.png",
-      link: "https://satyaki7.github.io/DigiCard/",
-      title: "Portfolio",
-      description: "This is my Portfolio. Check it out!",
-    },
-    {
-      image: "./src/assets/twitter.jpg",
-      link: "https://x.com/Satyaki30527321",
-      title: "Twitter",
-      description: "Not so active here.",
-    },
-    {
-      image: "./src/assets/github.webp",
-      link: "https://github.com/Satyaki7",
-      title: "Github",
-      description: "See all my projects here.",
-    },
-  ];
+ const items = [
+   {
+     image: gmailImg,
+     link: "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=dsatyaki08@gmail.com",
+     title: "Mail",
+     description: "Reach me through mail.",
+   },
+   {
+     image: linkedinImg,
+     link: "https://www.linkedin.com/in/satyaki-das7/",
+     title: "Linkedin",
+     description: "This is where I post my work.",
+   },
+   {
+     image: discordImg,
+     link: "https://google.com/",
+     title: "Discord",
+     description: "This is where I chill with the community.",
+   },
+   {
+     image: instagramImg,
+     link: "https://www.instagram.com/satyaki_das.official/",
+     title: "Instagram",
+     description: "Follow me on Instagram",
+   },
+   {
+     image: leetcodeImg,
+     link: "https://leetcode.com/u/satyaki7/",
+     title: "Leetcode",
+     description: "Want to see my DSA skills?",
+   },
+   {
+     image: portfolioImg,
+     link: "https://satyaki7.github.io/DigiCard/",
+     title: "Portfolio",
+     description: "This is my Portfolio. Check it out!",
+   },
+   {
+     image: twitterImg,
+     link: "https://x.com/Satyaki30527321",
+     title: "Twitter",
+     description: "Not so active here.",
+   },
+   {
+     image: githubImg,
+     link: "https://github.com/Satyaki7",
+     title: "Github",
+     description: "See all my projects here.",
+   },
+ ];
 
   return (
     <div style={{ height: "100%", width: "100%" }}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts or vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Profile-links/',
   plugins: [react()],
-})
+});
